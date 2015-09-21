41b4f8c4b30d0ed646362da289db226b06333bdf
Aaron Ogan <aaron@stackwave.com>
2015-09-21 13:48:05 -0500

    Example of 'react-tab-panel' component

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    publicPath: 'http://localhost:8090/assets'
+  },
+  module: {
+    loaders: [{
+        test: /\.jsx$/,
+        loader: 'babel'
+    }]
+  },
+  externals: {
+    'react': 'React'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
